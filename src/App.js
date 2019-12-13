b8b2370ce847f2b667881704268f92ff197b2021
aurkus <eredar21@gmail.com>
2019-12-13 01:20:38 -0400

    added functionality and components for Items, add Item form Item show list and the Header of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import ItemsList from './components/ItemsList';
+import uuid from 'uuid/v4';
+import Header from './components/Header';
+
+const initialItems = [
+  {id:uuid(), type:"inc", description:"pagamento mensual", amount:2400},
+  {id:uuid(), type:"exp", description:"Alugel", amount:370}
+]
+
+
 
 function App() {
+
+  const [items, setItems] = useState(initialItems);
+  const [type, setType] = useState("inc");
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+
+
+  // *********** functionality **************
+  
+  //add type
+  const updateType = e => {
+    setType(e.target.value);
+  };
+
+  //add descriptrion
+  const updateDescription = e => {
+    setDescription(e.target.value);
+  };
+
+  // add amount
+  const updateAmount = e => {
+    let amount = e.target.value;
+    if (amount === "") {
+      setAmount(amount);
+    } else {
+      setAmount(parseInt(amount));
+    }
+  };
+  
+  const deleteItem = id => {
+    let tempItems = items.filter(item => item.id !== id);
+    setItems(tempItems);
+    
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+    const singleItem = { id: uuid(), type, description, amount };
+    setItems([...items, singleItem]);
+    // set charge back to empty string
+    setDescription("");
+    // set amount back to zero
+    setAmount("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">
+      <div className="top">
+        <Header items={items}/>
+      </div>
+      <div className="botom">
+        <Form 
+          onSubmit={submitHandler}
+          type={type}
+          updateType={updateType}
+          description={description}
+          updateDescription={updateDescription}
+          amount={amount}
+          updateAmount={updateAmount}
+        />
+        <ItemsList items={items} deleteItem={deleteItem}/>
+      </div>
     </div>
   );
 }
